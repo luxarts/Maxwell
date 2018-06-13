da165ea400b08b86294cc3aa54775f59d251f1b4
Lucas M. Bacelo <bacelolucas@gmail.com>
2018-06-13 07:35:25 -0300

    compress update

--- Software/WIFI/compress.py
+++ Software/WIFI/compress.py
@@ -8,4 +8,4 @@ for f in fileList:
 	print ("Comprimiendo "+f)
 	with open('ESPWebSockets/data/'+f, 'rb') as f_in:
 		with gzip.open('ESPWebSockets/data/'+f+".gz", 'wb', 9) as f_out:
-			shutil.copyfileobj(f_replaced, f_out)
+			shutil.copyfileobj(f_in, f_out)
